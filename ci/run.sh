cbd85232b84e5f3350889deab0b20bc9e3e6ece3
Ryoma Kai <legknmj@icloud.com>
2018-05-20 08:58:14 +0900

    Add concourse pipeline (#2)
    * making pipeline!!!!
    - chenge dependncies(glide to dep)
    - make concourse pipeline
    - add cron-resource and set cron time to 7:10
    - change result kml name
    - add pre release
    - set slack message

---
+++ ci/run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e -u -x
+shopt -s dotglob
+
+# prepare
+INPUT_REPO=$PWD/repo
+GOPATH_REPO=$GOPATH/src/github.com/legnoh/wlw-locate-kml
+mkdir -p $GOPATH/src/github.com/legnoh
+mv $INPUT_REPO $GOPATH_REPO
+cd $GOPATH_REPO
+dep ensure
+
+# run
+go run main.go
+
+# output
+cd ..
+cp $GOPATH_REPO $INPUT_REPO
+cp $INPUT_REPO/result-*.kml out/
+
+
+# make release info
+date +%Y/%m/%d > out/name
+date +%Y%m%d > out/tag
+
+wget https://github.com/legnoh/wlw-locate-kml/releases/download/`date -d '-1 month' +%Y%m01`/result-`date -d '-1 month' +%Y%m01`.kml
+
+diff result-`date -d '-1 month' +%Y%m01`.kml result-`date +%Y%m%d`.kml \
+--ignore-matching-lines=".*name.*" \
+--ignore-matching-lines=".*description.*" \
+--ignore-matching-lines=".*coordinates.*" \
+--ignore-matching-lines ".*ランキング.*" \
+--ignore-matching-lines ".*styleUrl.*" \
+-U 0 > out/diff
+
+echo "# 新規出店・退店\n\n\`\`\`diff\n" > out/body
+cat out/diff >> out/body
+echo "\`\`\`" >> out/body
